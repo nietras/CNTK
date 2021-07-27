fffd226dd649fa1d964cb1e160cc8091f2c6a5ae
ntr <ntr@ihfood.dk>
2021-07-27 13:30:11 +0200

    Try fix packages

--- PackageSpecs/pack.ps1
+++ PackageSpecs/pack.ps1
@@ -3,4 +3,4 @@ dotnet pack -c Release  /p:Platform=x64 "..\bindings\csharp\CNTKLibraryManagedDl
 ./nuget.exe pack nietras.CNTK.Deps.Cuda/nietras.CNTK.Deps.Cuda.nuspec
 ./nuget.exe pack nietras.CNTK.Deps.MKL/nietras.CNTK.Deps.MKL.nuspec
 ./nuget.exe pack nietras.CNTK.Deps.OpenCV.Zip/nietras.CNTK.Deps.OpenCV.Zip.nuspec
-./nuget.exe pack nietras.CNTK.Deps.cuDNN/nietras.CNTK.Deps.cuDNN.nuspecd
+./nuget.exe pack nietras.CNTK.Deps.cuDNN/nietras.CNTK.Deps.cuDNN.nuspec
