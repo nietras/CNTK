4881c7f3ae74229354a7a87b84bae6fddc71228f
nietras <nietras@users.noreply.github.com>
2023-06-06 10:55:34 +0200

    add generate-proto-code.ps1 script and regenerate (fix casing), try fix vcxproj path to protoc too.

--- generate-proto-code.ps1
+++ generate-proto-code.ps1
@@ -1,2 +1,2 @@
 #!/usr/bin/env pwsh
-C:\git\oss\CNTK\vcpkg_installed\x64-windows\tools\protobuf\protoc.exe Source\CNTKv2LibraryDll\proto\CNTK.proto --cpp_out=Source\CNTKv2LibraryDll\proto
+C:\git\oss\CNTK\vcpkg_installed\x64-windows\tools\protobuf\protoc.exe --proto_path=Source\CNTKv2LibraryDll\proto --cpp_out=Source\CNTKv2LibraryDll\proto CNTK.proto
