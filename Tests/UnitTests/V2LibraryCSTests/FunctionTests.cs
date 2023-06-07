31e5c4725994dec63289e83920a28614c3621a28
nietras <nietras@users.noreply.github.com>
2023-06-07 11:14:55 +0200

    refactor FunctionTests a bit

--- Tests/UnitTests/V2LibraryCSTests/FunctionTests.cs
+++ Tests/UnitTests/V2LibraryCSTests/FunctionTests.cs
@@ -15,7 +15,7 @@ namespace CNTK.V2LibraryCSTests
         public void TestSaveAndLoad()
         {
             int channels = 2;
-            int imageWidth = 40, imageHeight = 40;
+            int imageWidth = 32, imageHeight = 16;
             int[] inputDim = { imageHeight, imageWidth, channels };
             Variable input = CNTKLib.InputVariable(inputDim, DataType.Float, "Images");
             Parameter param = new Parameter(inputDim, DataType.Float, CNTKLib.GlorotUniformInitializer(0.1F, 1, 0), DeviceDescriptor.CPUDevice);
@@ -29,7 +29,26 @@ namespace CNTK.V2LibraryCSTests
         }
 
         [TestMethod]
-        public void TestSaveAndLoadOnnx()
+        public void TestSaveAndLoadToFile()
+        {
+            int channels = 2;
+            int imageWidth = 32, imageHeight = 16;
+            int[] inputDim = { imageHeight, imageWidth, channels };
+            Variable input = CNTKLib.InputVariable(inputDim, DataType.Float, "Images");
+            Parameter param = new Parameter(inputDim, DataType.Float, CNTKLib.GlorotUniformInitializer(0.1F, 1, 0), DeviceDescriptor.CPUDevice);
+            Function model = CNTKLib.Plus(input, param, "Plus");
+            string savedFileName = "./TestSaveAndLoadToFileSavedModel.txt";
+            model.Save(savedFileName);
+            Function loadedModel = Function.Load(savedFileName, DeviceDescriptor.CPUDevice);
+            File.Delete(savedFileName);
+            Assert.AreEqual(model.Name, loadedModel.Name);
+            Assert.AreEqual(model.Inputs.Count, loadedModel.Inputs.Count);
+            Assert.AreEqual(model.Inputs[0].Shape, loadedModel.Inputs[0].Shape);
+            Assert.AreEqual(model.Output.Shape, loadedModel.Output.Shape);
+        }
+
+        [TestMethod]
+        public void TestSaveAndLoadToFileOnnx()
         {
             int channels = 2;
             int imageWidth = 32;
@@ -39,7 +58,7 @@ namespace CNTK.V2LibraryCSTests
             Variable input = CNTKLib.InputVariable(inputDim, DataType.Float, "Images");
             Parameter param = new Parameter(inputDim, DataType.Float, CNTKLib.GlorotUniformInitializer(0.1F, 1, 0), DeviceDescriptor.CPUDevice);
             Function model = CNTKLib.Plus(input, param, "Plus");
-            const string fileName = nameof(TestSaveAndLoadOnnx) + ".onnx";
+            const string fileName = nameof(TestSaveAndLoadToFileOnnx) + ".onnx";
             var format = ModelFormat.ONNX;
             model.Save(fileName, format);
             Function loadedModel = Function.Load(fileName, DeviceDescriptor.CPUDevice, format);
@@ -47,26 +66,6 @@ namespace CNTK.V2LibraryCSTests
             Assert.AreEqual(model.Inputs.Count, loadedModel.Inputs.Count);
             CollectionAssert.AreEqual(expectedOnnxDims, loadedModel.Inputs[0].Shape.Dimensions.ToArray());
             CollectionAssert.AreEqual(expectedOnnxDims, loadedModel.Output.Shape.Dimensions.ToArray());
-        }
-
-        [TestMethod]
-        public void TestSaveAndLoadToFile()
-        {
-            int channels = 2;
-            int imageWidth = 40, imageHeight = 40;
-            int[] inputDim = { imageHeight, imageWidth, channels };
-            Variable input = CNTKLib.InputVariable(inputDim, DataType.Float, "Images");
-            Parameter param = new Parameter(inputDim, DataType.Float, CNTKLib.GlorotUniformInitializer(0.1F, 1, 0), DeviceDescriptor.CPUDevice);
-            Function model = CNTKLib.Plus(input, param, "Minus");
-            string savedFileName = "./TestSaveAndLoadToFileSavedModel.txt";
-            model.Save(savedFileName);
-            byte[] buffer = model.Save();
-            Function loadedModel = Function.Load(savedFileName, DeviceDescriptor.CPUDevice);
-            File.Delete(savedFileName);
-            Assert.AreEqual(model.Name, loadedModel.Name);
-            Assert.AreEqual(model.Inputs.Count, loadedModel.Inputs.Count);
-            Assert.AreEqual(model.Inputs[0].Shape, loadedModel.Inputs[0].Shape);
-            Assert.AreEqual(model.Output.Shape, loadedModel.Output.Shape);
         }
 
         [TestMethod]
