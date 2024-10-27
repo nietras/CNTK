460d7f00879700b6205db16b98c927bfcba76bef
nietras <nietras@users.noreply.github.com>
2024-10-27 11:43:32 +0100

    Revise compute targets and add sm_70, sm_89 (#12)

--- PackageSpecs/push-azure.ps1
+++ PackageSpecs/push-azure.ps1
@@ -1,1 +1,1 @@
-./nuget.exe push *2.9.0.nupkg -Timeout 1200 -Source https://SITE.pkgs.visualstudio.com/_packaging/PROJECT/nuget/v3/index.json -ApiKey az
+./nuget.exe push *2.9.1.1.nupkg -Timeout 1200 -Source https://SITE.pkgs.visualstudio.com/_packaging/PROJECT/nuget/v3/index.json -ApiKey az
--- PackageSpecs/push-github.ps1
+++ PackageSpecs/push-github.ps1
@@ -1,1 +1,1 @@
-./nuget.exe push *2.9.0.nupkg -Timeout 1200 -Source https://nuget.pkg.github.com/nietras/index.json -ApiKey <PAT>
+./nuget.exe push *2.9.1.nupkg -Timeout 1200 -Source https://nuget.pkg.github.com/nietras/index.json -ApiKey <PAT>
